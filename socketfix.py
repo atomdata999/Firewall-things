be3f0e5269d2a42d5cb6ba7750249c8536ece4ed
atomdata999 <100035372+atomdata999@users.noreply.github.com>
2022-04-28 16:08:38 -0400

    Update socketfix.py

--- socketfix.py
+++ socketfix.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 #something simple
 #blocks all socket methods
 #https://unix.stackexchange.com/questions/139285/limit-max-connections-per-ip-address-and-new-connections-per-second-with-iptable this can help you
@@ -7,7 +8,15 @@ import sys
 #reverse proxy https://stackoverflow.com/questions/35863265/reverse-proxy-iptables
 #this is good to https://www.cyberciti.biz/faq/linux-kernel-etcsysctl-conf-security-hardening/
 # this is only if you can't whitelist a certain application or is to much of a hastle to do so
-os.system("netstat -n -p | grep SYN_REC | awk '{print $5}' | awk -F: '{print $1}' | sort | uniq >> black.txt")
-ips = open("black.txt", "r").readlines()
-for ip in ips:
-	os.system(f"iptables -I INPUT -s {ip} -j DROP")
+while True:
+	print("BLOCKING IPS")
+	os.system("netstat -n -p | grep SYN_REC | awk '{print $5}' | awk -F: '{print $1}' | sort | uniq >> black.txt")
+	ips = open("black.txt", "r").readlines()
+	for ip in ips:
+		if ip == "":
+			print("NOT BEING ATTACKED!!!")
+		elif ip == "your ip":
+			print("NOT BEING ATTACKED!!!")
+		else:
+			print("Has been attacked!!!")
+			os.system(f"iptables -I INPUT -s {ip} -j DROP")
